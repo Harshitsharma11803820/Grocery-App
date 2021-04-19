e94550efffe7600770a15bc1f66da6219b376b56
Harshitsharma11803820 <73836524+Harshitsharma11803820@users.noreply.github.com>
2021-04-19 23:23:05 +0530

    Update create.blade.php

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -4,19 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TODO | Create</title>
+    <link rel="stylesheet" href="css/app.css">
+    <title></title>
 </head>
 <body style="text-align:center">
-    <h1>Create your TODO</h1>
+<div class="wrapper-grid">
+            <div class="container-grid">
+    <h1>Add Item</h1>
     <h3>
         <x-alert />
     </h3>
     <form action="/upload" method="post">
         @csrf
-        <input type="text" name="title" />
-        <input type="submit" value="Create" />
+        <label for="title">Choose an item</label>
+
+<select name="title" type="text" id="title" style="padding:10px 20px;">
+  <option value="tea">Tea</option>
+  <option value="coffee">Coffee</option>
+  <option value="apple">Apple</option>
+  <option value="banana">Banana</option>
+</select><br><br>
+        <!-- <input type="text" class="text-edit" name="title" /> <br><br> -->
+        <input type="submit" class="edit-btn" value="Create" /><br><br>
     </form>
     <br>
-    <a href="/index">Back</a>
+    <a href="/index" class="back-btn" >Home</a> <br><br><br><br>
+    </div>
+    </div>
 </body>
 </html>
